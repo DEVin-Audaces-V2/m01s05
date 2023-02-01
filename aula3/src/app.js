ca51a6970d1f060cb1c517ece73a25e5780a42bb
romeuic <casarottori@gmail.com>
2023-02-01 20:11:32 -0300

    atualiza exemplo aula 3

--- aula3/src/app.js
+++ aula3/src/app.js
@@ -20,11 +20,11 @@ class Retangulo {
 
 const quad = new Retangulo(4, 111111);
 //quad.altura = 44444;
-console.log(quad.altura);
-console.log(quad.blah);
+//console.log(quad.altura);
+//console.log(quad.blah);
 
 //console.log(quad);
-console.log(quad.calculaArea());
+//console.log(quad.calculaArea());
 
 
 // EXEMPLO de Getter em Objeto Comum
@@ -36,3 +36,12 @@ const objetoComum = {
 };
 //console.log(objetoComum.valorInterno);
 //console.log(objetoComum.valor);
+
+import Fatura from "./Fatura.js";
+
+const melao = new Fatura(123, "Melão", 2, 3);
+
+const valor = melao.obterValorTotal();
+
+console.log(valor); // 6
+console.log(melao.valorTotal);
