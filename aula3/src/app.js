ebbe2f3f6b2dcf446268c30e4ffaf355734cf52b
romeuic <casarottori@gmail.com>
2023-02-01 21:38:06 -0300

    atualiza exemplo aula 3

--- aula3/src/app.js
+++ aula3/src/app.js
@@ -2,8 +2,10 @@ console.log('aula3');
 
 // EXEMPLO Getter em Classe
 class Retangulo {
-  #base
-  #altura
+  #base;
+  #altura;
+  exemploNaoStatic = "Pertenço a instância";
+  static exemploStatic = "Pertenço a Retangulo";
 
   constructor(b, a) {
     this.#base = b;
@@ -20,15 +22,20 @@ class Retangulo {
     console.log("executou o Setter");
     this.#altura = novoValor;
   }
+  static isRetangulo() {
+    console.log("testa se é instancia de retangulo");
+  }
 }
 
 const quad = new Retangulo(4, 111111);
-//quad.altura = 44444;
-quad.altura = 77777777777;
-console.log(quad.altura);
-//console.log(quad.blah);
-console.log(quad);
-//console.log(quad.calculaArea());
+//quad.altura = 77777777777;
+// console.log(quad.altura);
+// quad.exemploStatic = 234;
+// console.log(quad);
+// console.log(quad.calculaArea());
+// console.log(quad.exemploNaoStatic);
+// console.log(Retangulo.exemploStatic);
+// Retangulo.isRetangulo()
 
 
 // EXEMPLO de Getter/Setter em Objeto Comum
@@ -52,3 +59,39 @@ const melao = new Fatura(123, "Melão", 2, 3);
 const valor = melao.obterValorTotal();
 //console.log(valor); // 6
 //console.log(melao.valorTotal);
+
+
+// EXEMPLOS de atributos e métodos STATIC
+
+
+class Item {
+  static #nextId = 0;
+  id;
+  label;
+  batata;
+
+  constructor(label) {
+    this.id = Item.#nextId++;
+    this.label = label;
+  }
+
+  static isItem(parametro) {
+    return parametro instanceof Item;
+  }
+}
+
+const item1 = new Item('batata');
+const item2 = new Item('mamão');
+const item3 = new Item('banana');
+
+console.log(item1);
+// console.log(item2);
+// console.log(item3);
+
+
+const vetor = new Array();
+//vetor.isArray(); // isso não existe
+//console.log(Array.isArray(vetor));
+
+console.log(item1 instanceof Item);
+console.log(Item.isItem(item2));
