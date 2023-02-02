ec07442811aa2c7b5ff56b50329bf42f5994faaf
romeuic <casarottori@gmail.com>
2023-02-01 21:52:54 -0300

    atualiza projetinho

--- projetinho/src/app.js
+++ projetinho/src/app.js
@@ -1,31 +1,47 @@
-console.log("funciona");
-
 const campoTitulo = document.getElementById("campo-titulo");
 const botaoSalvar = document.getElementById("botao-salvar");
 const listaItens = document.getElementById("lista-itens");
 const formulario = document.getElementById("formulario");
 
-const vetorItens = [
+let vetorItens = [
   "item 0",
   "outro item",
   "e mais um",
 ];
 
+function apagaItem(itemParaApagar) {
+  vetorItens = vetorItens.filter(item => item !== itemParaApagar);
+  atualizaTela();
+}
+
+function criaElementoItem(item) {
+  const li = document.createElement("li");
+  li.innerHTML = item;
+  const botaoApagar = document.createElement("button");
+  botaoApagar.innerHTML = "x";
+  botaoApagar.addEventListener("click", () => {
+    apagaItem(item);
+  });
+  li.appendChild(botaoApagar);
+  return li;
+}
+
 function atualizaTela() {
   listaItens.innerHTML = "";
 
   vetorItens.forEach(item => {
-    const li = document.createElement("li");
-    li.innerHTML = item;
-    listaItens.appendChild(li);
+    const elemento = criaElementoItem(item);
+    listaItens.appendChild(elemento);
   });
 }
 
 function adicionaItem() {
   const titulo = campoTitulo.value;
-  vetorItens.push(titulo);
-  atualizaTela();
-  campoTitulo.value = "";
+  if (titulo) {
+    vetorItens.push(titulo);
+    atualizaTela();
+    campoTitulo.value = "";
+  }
 }
 
 formulario.addEventListener("submit", (event) => {
