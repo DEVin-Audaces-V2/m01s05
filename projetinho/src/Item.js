97d6d1a8823592f9aac794ce61bf780d26de5d23
romeuic <casarottori@gmail.com>
2023-02-02 21:14:10 -0300

    atualiza exemplo projetinho

---
+++ projetinho/src/Item.js
@@ -0,0 +1,15 @@
+
+export default class Item {
+  static #lastId = 0;
+  #id;
+  label;
+
+  constructor(label) {
+    this.#id = ++Item.#lastId;
+    this.label = label;
+  }
+
+  get id() {
+    return this.#id;
+  }
+}
